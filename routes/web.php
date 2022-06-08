a5d142076ef137a447217525df5019d80d052765
muccidavide <davidemucci0804@gmail.com>
2022-06-08 14:32:00 +0200

    Add Home and dynamic data

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $name = 'Davide Mucci';
+    $job = 'Junior Full Stack Web Developer';
+    return view('home', compact('name', 'job'));
 });
